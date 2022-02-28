65cd29ddf471bb3fde58d9ea4eb90c26564a49a8
Andrea Campo <campo@euris.it>
2022-02-28 10:37:04 +0100

    Add project files.

---
+++ CVPageInCSS/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CVPageInCSS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
